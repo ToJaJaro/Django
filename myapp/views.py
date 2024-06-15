5e1ecd9dcedf45da9b58b62df833707fd405b5d2
ToJaJaro <karoljarosz51@gmail.com>
2024-06-15 17:42:24 +0200

    okok

---
+++ myapp/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from .models import MyModel
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def list(request):
+    items = MyModel.objects.all()
+    return  render(request, 'list.html', {"list" : items})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
